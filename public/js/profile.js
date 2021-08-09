7e9e9d997b139b186f85a1508d35571ded826c5e
BN0b13 <NobleNotes1@gmail.com>
2021-08-09 06:37:30 -0700

    Worked on Edit function

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,35 +1,31 @@
 document.addEventListener('DOMContentLoaded', loadUp);
 const postTitle = document.getElementById('title');
 const postBody = document.getElementById('body');
-const submitBtn = document.getElementById('submitBtn');
+const modalBtnArea = document.getElementById('modalBtnArea');
 const profileContent = document.getElementById('profileContent');
 const editBtnArea = document.getElementById('editBtnArea');
 const logOutBtn = document.getElementById('logOutBtn');
 
 function profileControlls() {
-  profileContent.addEventListener('click', (e) => {
-    
+  profileContent.addEventListener('click', (e) => {    
     const targ = e.target.parentElement.parentElement.parentElement.parentElement.parentElement.id;
-    
-    console.log(targ);
+
       if(e.target.className == 'deleteBtn btn btn-danger btn-sm') {
         fetch(`/${targ}`, {
           method: 'DELETE'
         }).then(response=> response.json())
-        .then(console.log(`Deleted ${targ}`));
-        loadContent();
+        .then(data => {
+          return loadUp();
+        });
       } else if(e.target.className == 'editBtn btn btn-secondary btn-sm') {
         const titleContent = e.target.parentElement.parentElement.parentElement.parentElement.parentElement.children[1].textContent;
         const bodyContent = e.target.parentElement.parentElement.parentElement.parentElement.parentElement.children[2].textContent;
         postTitle.value = titleContent;
         postBody.value = bodyContent;
-        // submitBtn.textContent = 'Edit Post';
-        // submitBtn.type = 'button';
-        // submitBtn.id = 'submitEdit';
-  
-        submitBtn.style.display = 'none';
-        editBtnArea.innerHTML = '<button type="button" id="submitEdit" class="btn btn-primary m-2" data-bs-dismiss="modal">Edit Post</button>';
-  
+        modalBtnArea.innerHTML = '<button type="button" id="postCancelBtn" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button><button type="button" id="submitEdit" class="btn btn-primary m-2" data-bs-dismiss="modal">Edit Post</button>';
+        document.getElementById('postCancelBtn').addEventListener('click', () => {
+          clearFields();
+        })
         document.getElementById('submitEdit').addEventListener('click', function() {
           const title = postTitle.value;
           const body = postBody.value;
@@ -43,10 +39,12 @@ function profileControlls() {
           body: JSON.stringify(data)
           
         }).then(response=> response.json())
-        .then(clearFields())
+        .then(data => {
+          return loadContent();
+        })
         });
       } else { return }
-  
+
     e.preventDefault();
   });
 }
@@ -54,7 +52,7 @@ function profileControlls() {
 function clearFields() {
   postTitle.value = '';
   postBody.value = '';
-  submitBtn.style.display = 'block';
+  modalBtnArea.innerHTML = '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button><button type="submit" id="submitBtn" class="btn btn-primary m-2">Submit</button>';
 }
 
 async function loadContent() {
@@ -69,9 +67,9 @@ async function loadContent() {
             <div class="card-body" id="${post._id}">
               <div class="d-flex justify-content-end">
                 <div class="dropdown">
-                  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">Edit and Delete</button>
+                  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">Manage</button>
                   <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li><button class="editBtn btn btn-secondary btn-sm" data-bs-toggle="modal" data-bs-target="#newBlogPost">New Edit</button></li>
+                    <li><button class="editBtn btn btn-secondary btn-sm" data-bs-toggle="modal" data-bs-target="#newBlogPost">Edit</button></li>
                     <li><button type="button" class="deleteBtn btn btn-danger btn-sm">Delete</button></li>
                   </ul>
                 </div>
@@ -105,6 +103,7 @@ async function loadUser() {
 }
 
 function loadUp() {
+  clearFields();
   loadUser();
   loadContent();
   profileControlls();
